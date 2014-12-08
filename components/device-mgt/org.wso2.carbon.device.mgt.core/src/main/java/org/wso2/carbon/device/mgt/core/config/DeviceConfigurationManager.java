a6e9a9834e5ef9f3362d54df3dcdf86e6da025e5
prabathabey <prabathabey@git.com>
2014-12-08 18:58:25 +0530

    code cleanup

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/config/DeviceConfigurationManager.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/config/DeviceConfigurationManager.java
@@ -18,14 +18,10 @@ package org.wso2.carbon.device.mgt.core.config;
 import org.w3c.dom.Document;
 import org.wso2.carbon.device.mgt.common.DeviceManagementConstants;
 import org.wso2.carbon.device.mgt.common.DeviceManagementException;
-<<<<<<< HEAD
 import org.wso2.carbon.device.mgt.core.config.datasource.DataSourceConfig;
-=======
-import org.wso2.carbon.device.mgt.core.dao.DeviceMgtDAOFactory;
->>>>>>> 76e103ef003eec02e514b6885d2d4fbf4cd6fec8
 import org.wso2.carbon.device.mgt.core.util.DeviceManagerUtil;
 import org.wso2.carbon.utils.CarbonUtils;
-import javax.sql.DataSource;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
