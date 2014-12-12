274d9ec37e50140c6f624b002380441797fe3768
Geeth Munasinghe <geeth@wso2.com>
2014-12-12 14:45:24 +0530

    Adding the feature management service and android application to webapps

--- components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/spi/PolicyManagerService.java
+++ components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/spi/PolicyManagerService.java
@@ -30,6 +30,7 @@ public interface PolicyManagerService {
 
     /**
      * This method adds a policy to the platform
+     *
      * @param policy
      * @return primary key (generated key)
      */
@@ -38,6 +39,7 @@ public interface PolicyManagerService {
 
     /**
      * This method adds a policy per device which should be implemented by the related plugins.
+     *
      * @param deviceId
      * @param deviceType
      * @param policy
@@ -48,15 +50,17 @@ public interface PolicyManagerService {
 
     /**
      * This method adds a policy to device type by the related device type plugins.
+     *
      * @param deviceType
      * @param policy
      * @return primary key (generated key)
      */
 
-    int addPolicyToDeviceType(String deviceType,Policy policy) throws FeatureManagementException, PolicyManagementException;
+    int addPolicyToDeviceType(String deviceType, Policy policy) throws FeatureManagementException, PolicyManagementException;
 
     /**
      * This method adds the policy to specific role.
+     *
      * @param roleName
      * @param policy
      * @return primary key (generated key)
@@ -65,30 +69,34 @@ public interface PolicyManagerService {
 
     /**
      * This method returns the policy of whole platform
+     *
      * @return
      */
 
-     Policy getPolicy();
+    Policy getPolicy();
 
     /**
      * This method gives the device specific policy.
+     *
      * @param deviceId
      * @param deviceType
-     * @return  Policy
+     * @return Policy
      */
 
     Policy getPolicyOfDevice(String deviceId, String deviceType) throws FeatureManagementException, PolicyManagementException;
 
     /**
      * This method returns the device type specific policy.
+     *
      * @param deviceType
-     * @return  Policy
+     * @return Policy
      */
 
     Policy getPolicyOfDeviceType(String deviceType) throws FeatureManagementException, PolicyManagementException;
 
     /**
      * This method returns the role specific policy.
+     *
      * @param roleName
      * @return
      */
@@ -96,4 +104,11 @@ public interface PolicyManagerService {
     Policy getPolicyOfRole(String roleName) throws FeatureManagementException, PolicyManagementException;
 
 
+    boolean isPolicyAvailableForDevice(String deviceId, String deviceType) throws PolicyManagementException;
+
+
+    boolean isPolicyUsed(String deviceId, String deviceType) throws PolicyManagementException;
+
+
+    void setPolicyUsed(String deviceId, String deviceType, Policy policy) throws PolicyManagementException;
 }
