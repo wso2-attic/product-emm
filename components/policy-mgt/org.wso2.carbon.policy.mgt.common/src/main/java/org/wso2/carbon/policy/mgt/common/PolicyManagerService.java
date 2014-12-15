bd5971d26338c9c245af7811387d59028fa51b98
Geeth Munasinghe <geeth@wso2.com>
2014-12-15 20:11:19 +0530

    Comminying policy management related code

--- components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/PolicyManagerService.java
+++ components/policy-mgt/org.wso2.carbon.policy.mgt.common/src/main/java/org/wso2/carbon/policy/mgt/common/PolicyManagerService.java
@@ -16,7 +16,7 @@
 * under the License.
 */
 
-package org.wso2.carbon.policy.mgt.common.spi;
+package org.wso2.carbon.policy.mgt.common;
 
 import org.wso2.carbon.policy.mgt.common.FeatureManagementException;
 import org.wso2.carbon.policy.mgt.common.Policy;
