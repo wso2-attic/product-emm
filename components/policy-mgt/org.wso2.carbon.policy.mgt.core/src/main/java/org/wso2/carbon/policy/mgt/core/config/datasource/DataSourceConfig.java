cbe13288d081b304edb9da7d5c6ac7e22f2546aa
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-01-19 15:08:42 +0530

    [1] Copyright notices of policy-mgt component classes were updated. [2] Mispelled getJndiLookupDefinition() of org.wso2.carbon.policy.mgt.core.config.datasource.DataSourceConfig was updated.

--- components/policy-mgt/org.wso2.carbon.policy.mgt.core/src/main/java/org/wso2/carbon/policy/mgt/core/config/datasource/DataSourceConfig.java
+++ components/policy-mgt/org.wso2.carbon.policy.mgt.core/src/main/java/org/wso2/carbon/policy/mgt/core/config/datasource/DataSourceConfig.java
@@ -1,10 +1,13 @@
 /*
  * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,15 +27,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "DataSourceConfiguration")
 public class DataSourceConfig {
 
-	private JNDILookupDefinition jndiLookupDefintion;
+	private JNDILookupDefinition jndiLookupDefinition;
 
 	@XmlElement(name = "JndiLookupDefinition", nillable = true)
-	public JNDILookupDefinition getJndiLookupDefintion() {
-		return jndiLookupDefintion;
+	public JNDILookupDefinition getJndiLookupDefinition() {
+		return jndiLookupDefinition;
 	}
 
-	public void setJndiLookupDefintion(JNDILookupDefinition jndiLookupDefintion) {
-		this.jndiLookupDefintion = jndiLookupDefintion;
+	public void setJndiLookupDefinition(JNDILookupDefinition jndiLookupDefinition) {
+		this.jndiLookupDefinition = jndiLookupDefinition;
 	}
-
 }
