63a8595f7cf0b5fc5e37101c4ab32e7dfffcb918
Dulitha Wijewantha <dulithaz@gmail.com>
2015-02-19 09:39:25 +0530

    iOS enrollment workflow - Added the UI templates to the relevant pages - Changed the theme - Added few generic components:- license-agreement-box, login-box, wizard - Used inheritence to overide the about mentioned components for enrollment

--- modules/distribution/src/repository/jaggeryapps/mdm/units/theme/public/js/main.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/theme/public/js/main.js
@@ -1,19 +1,26 @@
 $( document ).ready(function() {
 
-    $('[data-toggle="tooltip"]').tooltip();   
+    $('[data-toggle="tooltip"]').tooltip();
 
     $("[data-toggle=popover]").popover();
 
     $(".ctrl-asset-type-switcher").popover({
-        html : true, 
+        html : true,
         content: function() {
-          return $('#content-asset-types').html();
+            return $('#content-asset-types').html();
         }
     });
 
-	$('#nav').affix({
-	      offset: {
-	        top: $('header').height()
-	      }
-	});	 
+    $(".ctrl-filter-type-switcher").popover({
+        html : true,
+        content: function() {
+            return $('#content-filter-types').html();
+        }
+    });
+
+    $('#nav').affix({
+        offset: {
+            top: $('header').height()
+        }
+    });
 }); 
