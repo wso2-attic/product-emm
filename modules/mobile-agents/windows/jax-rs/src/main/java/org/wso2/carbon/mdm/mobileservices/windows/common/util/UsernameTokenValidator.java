0a799c133e8e98a5962de471e2ed5fb738767137
asok <asok@wso2.com>
2015-02-23 15:35:05 +0530

    replacing callbackhandler with a Validator

---
+++ modules/mobile-agents/windows/jax-rs/src/main/java/org/wso2/carbon/mdm/mobileservices/windows/common/util/UsernameTokenValidator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.mdm.mobileservices.windows.common.util;
+
+import org.apache.ws.security.WSSecurityException;
+import org.apache.ws.security.handler.RequestData;
+import org.apache.ws.security.validate.Credential;
+import org.apache.ws.security.validate.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UsernameTokenValidator implements Validator {
+
+	@Override public Credential validate(Credential credential, RequestData requestData)
+			throws WSSecurityException {
+
+		String user = credential.getUsernametoken().getName();
+		String password = credential.getUsernametoken().getPassword();
+
+		
+
+		return credential;
+	}
+}
