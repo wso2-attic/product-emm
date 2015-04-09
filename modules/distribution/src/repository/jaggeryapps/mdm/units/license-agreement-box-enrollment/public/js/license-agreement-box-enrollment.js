cf143b1affc1d3f4a25413980dd0ac1ac14d546b
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-09 21:35:25 +0530

    committing iOS enrollment related MDM UI updates

--- modules/distribution/src/repository/jaggeryapps/mdm/units/license-agreement-box-enrollment/public/js/license-agreement-box-enrollment.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/license-agreement-box-enrollment/public/js/license-agreement-box-enrollment.js
@@ -13,3 +13,49 @@ $( document ).ready(function() {
             });
         })(), 5000);
 });
+
+/**
+ * Following will be called when a user clicks
+ * on the "Add User" button.
+ */
+$("a.btn-download-agent").click(function(e){
+    e.preventDefault();
+
+    var enrollmentURL = "/mdm/controller/enroll";
+
+    $.ajax({
+        type: 'GET',
+        url: enrollmentURL,
+        success: function(data) {
+            if (data) {
+                // then device is an iOS device
+                var iOSResponse = JSON.parse(data);
+                setInterval(function(){
+                    var deviceCheckURL = iOSResponse.deviceCheckURL;
+                    var inputs = iOSResponse.inputs;
+
+                    $.ajax({
+                        type:'POST',
+                        url:deviceCheckURL,
+                        contentType:'application/json',
+                        data:JSON.stringify(inputs),
+                        success:function(data){
+                            var response = JSON.parse(data);
+                            var deviceID = response.deviceID;
+                            if(deviceID != null) {
+                                window.location.href = "wso2agent://" + deviceID;
+                            }
+                        },
+                        error:function(){
+                            alert("iOS enrollment error");
+                        }
+                    });
+                }, 5000);
+            }
+        },
+        error: function() {
+            alert("error in request to /mdm/controller/enroll");
+        }
+    });
+});
+
