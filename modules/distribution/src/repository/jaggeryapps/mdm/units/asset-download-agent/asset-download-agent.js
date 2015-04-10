0bf24fd3191498cc328a91b4a1281a50ca2352b7
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-10 11:00:04 +0530

    UI Updates to download iOS Agent

--- modules/distribution/src/repository/jaggeryapps/mdm/units/asset-download-agent/asset-download-agent.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/asset-download-agent/asset-download-agent.js
@@ -1,4 +1,6 @@
 function onRequest(context){
+    var mdmProps = require('/config/mdm-props.js').config();
+
     var log = new Log("asset-download-agent-unit");
     var userAgent = request.getHeader("User-Agent");
     var UAParser = require("/modules/ua-parser.min.js").UAParser;
@@ -11,10 +13,11 @@ function onRequest(context){
     if(os.name == "Android"){
         log.info("Android detected");
         viewModel.header = "Download and install Agent";
-        viewModel.link = "http://10.10.10.112:9763"+app.publicURL+"/asset/android-agent.apk";
+        viewModel.link = mdmProps.httpURL + app.publicURL+"/asset/android-agent.apk";
     }else if(os.name == "iOS"){
         viewModel.header = "Step 1. Download and install Agent";
-        viewModel.link = "itms-services://?action=download-manifest&url=itms-services://?action=download-manifest&url="+app.publicURL+"/asset/ios-agent.ipa";
+        viewModel.link = "itms-services://?action=download-manifest&url=itms-services://?action=download-manifest&url="
+            + mdmProps.httpURL + app.publicURL + "/asset/" + mdmProps.iOSAgentApp;
     }
     return viewModel;
 }
