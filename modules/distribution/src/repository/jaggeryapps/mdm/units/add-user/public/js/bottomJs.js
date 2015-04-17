0159327ce9e5854ea6687822f8c7e4bdebd94cc3
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-17 19:35:27 +0530

    Updates to Add User UI

--- modules/distribution/src/repository/jaggeryapps/mdm/units/add-user/public/js/bottomJs.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/add-user/public/js/bottomJs.js
@@ -1,14 +1,12 @@
 $( document ).ready(function() {
-
-    $('select.select2').select2({
-        placeholder: 'Select..'
+    $("select.select2").select2({
+        placeholder : "Select..."
     });
 
-    $('select.select2[multiple=multiple]').select2({
-        placeholder: 'Select..',
-        tags: true
+    $("select.select2[multiple=multiple]").select2({
+        placeholder : "Select...",
+        tags : true
     });
-
 });
 
 /**
@@ -24,11 +22,11 @@ var emailIsValid = function(email) {
 };
 
 /**
- * Following will be called when a user clicks
- * on the "Add User" button.
+ * Following click function would execute
+ * when a user clicks on "Add User" button
+ * on Add User page in WSO2 MDM Console.
  */
-$("button#add-user-btn").click(function(){
-    //e.preventDefault();
+$("button#add-user-btn").click(function() {
     var username = $("input#username").val();
     var firstname = $("input#firstname").val();
     var lastname = $("input#lastname").val();
@@ -36,20 +34,20 @@ $("button#add-user-btn").click(function(){
     var userRoles = $("select#roles").val();
 
     if (!username) {
-        $('.wr-validation-summary p').text("Username is a required field. It cannot be empty.");
-        $('.wr-validation-summary').removeClass("hidden");
+        $(".wr-validation-summary p").text("Username is a required field. It cannot be empty.");
+        $(".wr-validation-summary").removeClass("hidden");
     } else if (!firstname) {
-        $('.wr-validation-summary p').text("Firstname is a required field. It cannot be empty.");
-        $('.wr-validation-summary').removeClass("hidden");
+        $(".wr-validation-summary p").text("Firstname is a required field. It cannot be empty.");
+        $(".wr-validation-summary").removeClass("hidden");
     } else if (!lastname) {
-        $('.wr-validation-summary p').text("Lastname is a required field. It cannot be empty.");
-        $('.wr-validation-summary').removeClass("hidden");
+        $(".wr-validation-summary p").text("Lastname is a required field. It cannot be empty.");
+        $(".wr-validation-summary").removeClass("hidden");
     } else if (!emailAddress) {
-        $('.wr-validation-summary p').text("Email is a required field. It cannot be empty.");
-        $('.wr-validation-summary').removeClass("hidden");
+        $(".wr-validation-summary p").text("Email is a required field. It cannot be empty.");
+        $(".wr-validation-summary").removeClass("hidden");
     } else if (!emailIsValid(emailAddress)) {
-        $('.wr-validation-summary p').text("Email is not valid. Please enter a correct email address.");
-        $('.wr-validation-summary').removeClass("hidden");
+        $(".wr-validation-summary p").text("Email is not valid. Please enter a correct email address.");
+        $(".wr-validation-summary").removeClass("hidden");
     } else {
         var addUserFormData = {};
         addUserFormData.username = username;
@@ -61,25 +59,31 @@ $("button#add-user-btn").click(function(){
         var addUserAPI = "/mdm/api/users/add";
 
         $.ajax({
-            type:'POST',
-            url:addUserAPI,
-            contentType:'application/json',
-            data:JSON.stringify(addUserFormData),
-            success:function(data){
+            type : "POST",
+            url : addUserAPI,
+            contentType : "application/json",
+            data : JSON.stringify(addUserFormData),
+            success : function(data) {
                 if (data == 201) {
-                    $('.wr-validation-summary p').text("User created.");
+                    $(".wr-validation-summary p").text("User (" + username + ") was added.");
+                    // Clearing user input fields.
+                    $("input#username").val("");
+                    $("input#firstname").val("");
+                    $("input#lastname").val("");
+                    $("input#email").val("");
+                    $("select#roles").select2("val", "");
                 } else if (data == 400) {
-                    $('.wr-validation-summary p').text("Exception at backend.");
+                    $(".wr-validation-summary p").text("Exception occurred at backend.");
                 } else if (data == 403) {
-                    $('.wr-validation-summary p').text("Action not permitted.");
+                    $(".wr-validation-summary p").text("Action was not permitted.");
                 } else if (data == 409) {
-                    $('.wr-validation-summary p').text("User exists.");
+                    $(".wr-validation-summary p").text("Sorry, User already exists.");
                 }
-                $('.wr-validation-summary').removeClass("hidden");
+                $(".wr-validation-summary").removeClass("hidden");
             },
-            error:function(){
-                $('.wr-validation-summary p').text("An unexpected error occurred.");
-                $('.wr-validation-summary').removeClass("hidden");
+            error : function() {
+                $(".wr-validation-summary p").text("An unexpected error occurred.");
+                $(".wr-validation-summary").removeClass("hidden");
             }
         });
     }
