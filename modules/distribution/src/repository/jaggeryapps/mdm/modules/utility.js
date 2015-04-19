736c477e86b792ccec6c4240802e9aa1c2779b66
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-19 18:07:17 +0530

    Update/Refactor utility.js

--- modules/distribution/src/repository/jaggeryapps/mdm/modules/utility.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/modules/utility.js
@@ -6,54 +6,56 @@
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-var utility = (function () {
-    var module = {};
+var utility;
+utility = function () {
+    var JavaClass = Packages.java.lang.Class;
     var PrivilegedCarbonContext = Packages.org.wso2.carbon.context.PrivilegedCarbonContext;
-    var Class = java.lang.Class;
 
-    var osgiService = function (clazz) {
-        return PrivilegedCarbonContext.getThreadLocalCarbonContext().getOSGiService(Class.forName(clazz));
+    var getOsgiService = function (className) {
+        return PrivilegedCarbonContext.getThreadLocalCarbonContext().getOSGiService(JavaClass.forName(className));
     };
-    module.getDeviceManagementService = function () {
-        return osgiService('org.wso2.carbon.device.mgt.core.service.DeviceManagementService');
-    };
-    module.getUserManagementService = function(){
-        return osgiService('org.wso2.carbon.device.mgt.user.core.service.UserManagementService');
-    };
-    module.insertAppPermissions = function (userModule, type) {
-        userModule.addPermissions([{key: "device-mgt/", name: "Device Management"}], "", type);
-        userModule.addPermissions([{key: "device-mgt/admin", name: "Device Management Admin"}], "", type);
-        userModule.addPermissions([{key: "device-mgt/user", name: "Device Management User"}], "", type);
 
-        userModule.addPermissions([{key: "devices", name: "Device"}], "device-mgt/admin", type);
-        userModule.addPermissions([{key: "users", name: "User"}], "device-mgt/admin", type);
-        userModule.addPermissions([{key: "devices", name: "Device"}], "device-mgt/user", type);
+    var publicMethods = {};
 
-        userModule.addPermissions([{key: "devices/list", name: "List all Devices"}], "device-mgt/admin", type);
-        userModule.addPermissions([{key: "devices/operation", name: "Perform operations"}], "device-mgt/admin", type);
+    publicMethods.getDeviceManagementService = function () {
+        return getOsgiService('org.wso2.carbon.device.mgt.core.service.DeviceManagementService');
+    };
+
+    publicMethods.getUserManagementService = function () {
+        return getOsgiService('org.wso2.carbon.device.mgt.user.core.service.UserManagementService');
+    };
+
+    publicMethods.insertAppPermissions = function (userModule, type) {
+        userModule.addPermissions([{key: "device-mgt", name: "Device Management"}], "", type);
+        userModule.addPermissions([{key: "admin", name: "Device Management Admin"}], "device-mgt", type);
+        userModule.addPermissions([{key: "user", name: "Device Management User"}], "device-mgt", type);
+
+        userModule.addPermissions([{key: "devices", name: "Devices"}], "device-mgt/admin", type);
+        userModule.addPermissions([{key: "devices/list", name: "List Devices"}], "device-mgt/admin", type);
+        userModule.addPermissions([{key: "devices/operation", name: "Perform Operation"}], "device-mgt/admin", type);
+
+        userModule.addPermissions([{key: "users", name: "Users"}], "device-mgt/admin", type);
         userModule.addPermissions([{key: "users/list", name: "List Users"}], "device-mgt/admin", type);
         userModule.addPermissions([{key: "users/invite", name: "Invite Users"}], "device-mgt/admin", type);
         userModule.addPermissions([{key: "users/add", name: "Add New Users"}], "device-mgt/admin", type);
         userModule.addPermissions([{key: "users/remove", name: "Remove Users"}], "device-mgt/admin", type);
 
-        userModule.addPermissions([{key: "devices/list", name: "List own Devices"}], "device-mgt/user", type);
+        userModule.addPermissions([{key: "devices", name: "Devices"}], "device-mgt/user", type);
+        userModule.addPermissions([{key: "devices/list", name: "List Devices"}], "device-mgt/user", type);
+        userModule.addPermissions([{key: "devices/operation", name: "Perform Operation"}], "device-mgt/user", "init");
+    };
 
-        userModule.addPermissions([{
-            key: "devices/operation",
-            name: "Perform operations on own devices"
-        }], "device-mgt/user", "init");
-    }
-    return module;
-}());
+    return publicMethods;
+}();
 
 
