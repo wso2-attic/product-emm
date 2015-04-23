9ab8496264808c2cb617be9a7a4791d4fb874464
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-23 11:32:17 +0530

    Updated comments and formatting on add-user.js in add-user UI unit

--- modules/distribution/src/repository/jaggeryapps/mdm/units/add-user/add-user.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/add-user/add-user.js
@@ -1,8 +1,8 @@
 /**
- * Returns the state to be viewed by add-user page.
+ * Returns the dynamic state to be populated by add-user page.
  * 
- * @param context
- * @returns {*} A context object that returns the next state of this page to be presented
+ * @param context Object that gets updated with the dynamic state of this page to be presented
+ * @returns {*} A context object that returns the dynamic state of this page to be presented
  */
 function onRequest(context){
     var carbon = require('carbon');
@@ -12,10 +12,9 @@ function onRequest(context){
     var userManager = new carbon.user.UserManager(server, tenantId);
 
     var allRoles = userManager.allRoles();
-
     var i = 0;
     var filteredRoles = [];
-    while(allRoles[i]) {
+    while (allRoles[i]) {
         if (allRoles[i] != "Internal/subscriber" && allRoles[i] != "Internal/everyone") {
             filteredRoles.push(allRoles[i]);
         }
