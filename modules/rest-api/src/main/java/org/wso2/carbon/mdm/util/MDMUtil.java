e7d4f50ba59eed6ec8d0eaf4d271ae58f1f86318
mharindu <milanharindu.ucsc@gmail.com>
2015-05-14 22:23:51 +0530

    Added app manager supportive beans

--- modules/rest-api/src/main/java/org/wso2/carbon/mdm/util/MDMUtil.java
+++ modules/rest-api/src/main/java/org/wso2/carbon/mdm/util/MDMUtil.java
@@ -1,30 +1,48 @@
 package org.wso2.carbon.mdm.util;
 
-
-import org.wso2.carbon.device.mgt.common.Application;
 import org.wso2.carbon.device.mgt.common.operation.mgt.Operation;
 import org.wso2.carbon.device.mgt.core.operation.mgt.ProfileOperation;
 import org.wso2.carbon.mdm.api.common.MDMAPIException;
 import org.wso2.carbon.mdm.beans.MobileApp;
-import org.wso2.carbon.mdm.beans.android.InstallApplication;
+import org.wso2.carbon.mdm.beans.android.AppStoreApplication;
+import org.wso2.carbon.mdm.beans.android.EnterpriseApplication;
+import org.wso2.carbon.mdm.beans.android.WebApplication;
 
 public class MDMUtil {
 
     public static final String ANDROID_INSTALL_APPLICATION_OPCODE = "INSTALL_APPLICATION";
 
-    public static Operation createAndroidProfileOperation(MobileApp application) throws MDMAPIException{
+	public static Operation createAndroidProfileOperation(MobileApp application) throws MDMAPIException{
 
-        InstallApplication installApplication = new InstallApplication();
-        installApplication.setType(application.getType().toString());
-        installApplication.setAppIdentifier(application.getAppIdentifier());
-        installApplication.setUrl(application.getLocation());
-        ProfileOperation operation = new ProfileOperation();
-        operation.setCode(ANDROID_INSTALL_APPLICATION_OPCODE);
-        operation.setType(Operation.Type.PROFILE);
-        operation.setPayLoad(installApplication.toJSON());
+		ProfileOperation operation = new ProfileOperation();
+		operation.setCode(ANDROID_INSTALL_APPLICATION_OPCODE);
+		operation.setType(Operation.Type.PROFILE);
 
-        return operation;
-    }
+		switch (application.getType()) {
+			case ENTERPRISE:
+				EnterpriseApplication enterpriseApplication = new EnterpriseApplication();
+				enterpriseApplication.setType(application.getType().toString());
+				enterpriseApplication.setUrl(application.getLocation());
+				operation.setPayLoad(enterpriseApplication.toJSON());
+				break;
+			case PUBLIC:
+				AppStoreApplication appStoreApplication = new AppStoreApplication();
+				appStoreApplication.setType(application.getType().toString());
+				appStoreApplication.setAppIdentifier(application.getIdentifier());
+				operation.setPayLoad(appStoreApplication.toJSON());
+				break;
+			case WEBAPP:
+				WebApplication webApplication = new WebApplication();
+				webApplication.setUrl(application.getLocation());
+				webApplication.setName(application.getName());
+				operation.setPayLoad(webApplication.toJSON());
+				break;
+			default:
+				String errorMessage = "Invalid application type.";
+				throw new MDMAPIException(errorMessage);
+		}
+		return operation;
+	}
 
     public static Operation createIOSProfileOperation(MobileApp application){
 
