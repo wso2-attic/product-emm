fe75aa39ec527e4d5e94dd0fd68a645de8f91873
dilanua <dilan.u.ariyaratne@gmail.com>
2015-05-20 21:18:02 +0530

    Update user-listing view with status info

--- modules/distribution/src/repository/jaggeryapps/mdm/units/user-listing/user-listing.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/user-listing/user-listing.js
@@ -2,13 +2,19 @@ function onRequest(context) {
     // var log = new Log("user-listing");
     var userModule = require("/modules/user.js").userModule;
     var allUsers = userModule.getUsers();
-    var i, filteredUserList = [];
-    for (i = 0; i < allUsers.length; i++) {
-        if (String(allUsers[i].username) != "admin") {
-            filteredUserList.push(allUsers[i]);
+    if (allUsers.length == 1) {
+        context.users = [];
+        context.listUsersStatus = "No other Users found.";
+    } else {
+        var i, filteredUserList = [];
+        for (i = 0; i < allUsers.length; i++) {
+            if (String(allUsers[i].username) != "admin") {
+                filteredUserList.push(allUsers[i]);
+            }
         }
+        context.users = filteredUserList;
+        context.listUsersStatus = "Total number of Users found : " + filteredUserList.length;
     }
-    context.users = filteredUserList;
     context.permissions = userModule.getUIPermissions();
     return context;
 }
