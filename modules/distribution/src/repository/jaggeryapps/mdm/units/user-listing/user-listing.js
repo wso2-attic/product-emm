0441af0f85da3fd0251ede73e7757c4babe21de0
dilanua <dilan.u.ariyaratne@gmail.com>
2015-05-20 20:54:29 +0530

    Temporary fix for supper-admin-listing issue

--- modules/distribution/src/repository/jaggeryapps/mdm/units/user-listing/user-listing.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/user-listing/user-listing.js
@@ -1,6 +1,14 @@
-function onRequest(context){
+function onRequest(context) {
+    // var log = new Log("user-listing");
     var userModule = require("/modules/user.js").userModule;
-    context.users = userModule.getUsers();
+    var allUsers = userModule.getUsers();
+    var i, filteredUserList = [];
+    for (i = 0; i < allUsers.length; i++) {
+        if (String(allUsers[i].username) != "admin") {
+            filteredUserList.push(allUsers[i]);
+        }
+    }
+    context.users = filteredUserList;
     context.permissions = userModule.getUIPermissions();
     return context;
 }
