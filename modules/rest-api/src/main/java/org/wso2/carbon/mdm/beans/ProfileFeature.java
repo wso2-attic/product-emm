c90960ed442bf11f1bdaca702894d8f2b3c2af7c
manoj <manojg@wwso2.com>
2015-05-21 20:46:24 +0530

    Add Polciy wrapper for MDM level to handle JAX RS specific properties

---
+++ modules/rest-api/src/main/java/org/wso2/carbon/mdm/beans/ProfileFeature.java
@@ -0,0 +1,73 @@
+/*
+*  Copyright (c) 2015 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.mdm.beans;
+
+import org.codehaus.jackson.annotate.JsonRawValue;
+
+import java.io.Serializable;
+
+public class ProfileFeature implements Serializable {
+
+    private int id;
+    private String featureCode;
+    private int profileId;
+    private int deviceTypeId;
+    private Object content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFeatureCode() {
+        return featureCode;
+    }
+
+    public void setFeatureCode(String featureCode) {
+        this.featureCode = featureCode;
+    }
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(int profileId) {
+        this.profileId = profileId;
+    }
+
+    public int getDeviceTypeId() {
+        return deviceTypeId;
+    }
+
+    public void setDeviceTypeId(int deviceTypeId) {
+        this.deviceTypeId = deviceTypeId;
+    }
+
+    @JsonRawValue
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+}
