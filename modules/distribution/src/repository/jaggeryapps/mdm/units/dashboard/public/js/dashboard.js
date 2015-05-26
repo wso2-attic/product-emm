7114eb4221eb79b309a763350b16fcdbeed16c16
dilanua <dilan.u.ariyaratne@gmail.com>
2015-05-26 15:11:27 +0530

    Refactor Dashboard

--- modules/distribution/src/repository/jaggeryapps/mdm/units/dashboard/public/js/dashboard.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/dashboard/public/js/dashboard.js
@@ -1,14 +1,16 @@
-var updateStats = function(serviceURL, id){
-    invokerUtil.get(serviceURL,
-        function(data){
+var updateStats = function (serviceURL, id) {
+    invokerUtil.get(
+        serviceURL,
+        function (data) {
             $(id).html(data);
-        }, function(message){
+        }, function (message) {
             console.log(message);
-        });
-}
+        }
+    );
+};
 
-$(document).ready(function(){
-    updateStats("https://localhost:9443/mdm-admin/devices/count", "#deviceCount");
-    updateStats("https://localhost:9443/mdm-admin/policies/count", "#policyCount");
-    updateStats("https://localhost:9443/mdm-admin/users/count/" + "carbon.super", "#userCount");
+$(document).ready(function () {
+    updateStats("/mdm-admin/devices/count", "#device-count");
+    updateStats("/mdm-admin/users/count/" + "carbon.super", "#user-count");
+    updateStats("/mdm-admin/policies/count", "#policy-count");
 });
