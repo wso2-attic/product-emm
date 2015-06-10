4660be6b2a54c5ea6a37c8a4de28f799fc710e39
dilanua <dilan.u.ariyaratne@gmail.com>
2015-06-10 11:47:03 +0530

    Replaced Internal/everyone role with ANY @ policy-create-wizard

--- modules/distribution/src/repository/jaggeryapps/mdm/units/policy-create/policy-create.js
+++ modules/distribution/src/repository/jaggeryapps/mdm/units/policy-create/policy-create.js
@@ -1,7 +1,7 @@
 function onRequest(context) {
     var userModule = require("/modules/user.js").userModule;
 
-    context.roles = userModule.getRoles(true);
+    context.roles = userModule.getRoles(false);
     context.users = userModule.getUsers();
     context.actions = ["Enforce"];
 
