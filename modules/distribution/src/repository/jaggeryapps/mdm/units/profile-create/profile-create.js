d1f0e0d1a7a6be6a9242d51b53ed79252f87c5d2
dilanua <dilan.u.ariyaratne@gmail.com>
2015-06-24 20:53:56 +0530

    Deleted profile related UIs

--- modules/distribution/src/repository/jaggeryapps/mdm/units/profile-create/profile-create.js
+++
@@ -1,7 +0,0 @@
-function onRequest(context) {
-    var policyModule = require("/modules/policy.js").policyModule;
-    var policies = policyModule.getPolicies();
-    context.policies = policies;
-    context.deviceTypes = [{id: "1", name: "Android"},{id: "2", name: "iOS"},];
-    return context;
-}
