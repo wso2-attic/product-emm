914144352d82d8432474743bbaf2670e62abcbf4
manoj <manojg@wwso2.com>
2015-07-07 16:56:04 +0530

    Change get applist method name

--- modules/mobile-agents/android/jax-rs/src/main/java/org/wso2/carbon/mdm/services/android/DeviceManagementService.java
+++ modules/mobile-agents/android/jax-rs/src/main/java/org/wso2/carbon/mdm/services/android/DeviceManagementService.java
@@ -146,7 +146,7 @@ public class DeviceManagementService {
         deviceIdentifier.setType(DeviceManagementConstants.MobileDeviceTypes.MOBILE_DEVICE_TYPE_ANDROID);
         boolean result;
         try {
-            AndroidAPIUtils.getApplicationManagerService().updateApplicationListInstallInDevice(deviceIdentifier,
+            AndroidAPIUtils.getApplicationManagerService().updateApplicationListInstalledInDevice(deviceIdentifier,
                     applications);
             Response.status(Response.Status.ACCEPTED);
             responseMessage.setResponseMessage("Device information has modified successfully.");
