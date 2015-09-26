dbab9281a89912be1a7490408703d084483e4f9a
dilanua <dilan.u.ariyaratne@gmail.com>
2015-09-26 21:38:36 +0530

    Updating enrollment login error checks

--- modules/apps/jaggery/mdm/src/mdm/units/login-box-agent/public/js/login-box-agent.js
+++ modules/apps/jaggery/mdm/src/mdm/units/login-box-agent/public/js/login-box-agent.js
@@ -16,16 +16,71 @@
  * under the License.
  */
 
-$(document).ready(function () {
-    var currentHash = window.location.hash;
-    if (currentHash == "#auth-failed") {
-        $('.wr-validation-summary p').text("Sorry!, Please make sure to enter correct username and password");
-        $('.wr-validation-summary').removeClass("hidden");
-    } else if (currentHash == "#error") {
-        $('.wr-validation-summary p').text("Sorry!, Error occured");
-        $('.wr-validation-summary').removeClass("hidden");
+/**
+ * This method will return query parameter value given its name
+ * @param name Query parameter name
+ * @returns {string} Query parameter value
+ */
+var getParameterByName = function (name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+var errorMsgWrapper = "#enrollment-error-msg";
+var errorMsg = errorMsgWrapper + " span";
+
+/**
+ * This method will execute on login form submission and validate input.
+ * @returns {boolean}
+ */
+var validate = function () {
+    var username = $("input#username").val();
+    var password = $("input#password").val();
+
+    if (!username && !password) {
+        $(errorMsg).text("Both username and password are empty. You cannot proceed.");
+        if ($(errorMsgWrapper).hasClass("hidden")) {
+            $(errorMsgWrapper).removeClass("hidden");
+        }
+        return false;
+    } else if (!username && password) {
+        $(errorMsg).text("Username should not be empty.");
+        if ($(errorMsgWrapper).hasClass("hidden")) {
+            $(errorMsgWrapper).removeClass("hidden");
+        }
+        return false;
+    } else if (username && !password) {
+        $(errorMsg).text("Password should not be empty.");
+        if ($(errorMsgWrapper).hasClass("hidden")) {
+            $(errorMsgWrapper).removeClass("hidden");
+        }
+        return false;
+    } else {
+        return true;
     }
-    $('.btn-download-agent').click(function () {
-        $('.form-login-box').submit();
-    });
+};
+
+$(document).ready(function () {
+    var error = getParameterByName("error");
+    if (error == "auth-failed") {
+        var defaultMessage = "Please provide a correct username and password to continue.";
+        var customMessage = getParameterByName("message");
+        if (customMessage) {
+            $(errorMsg).text("Authentication failed. " + customMessage);
+        } else {
+            $(errorMsg).text("Authentication failed. " + defaultMessage);
+        }
+        $(errorMsgWrapper).removeClass("hidden");
+    } else if (error == "unexpected") {
+        $(errorMsg).text("An unexpected error occured. Please try again.");
+        $(errorMsgWrapper).removeClass("hidden");
+    }
 });
+
+$(".btn-download-agent").click(function () {
+    $(".form-login-box").submit();
+});
+
+
