72e0dc816d946206163904b6c1ec08ce568adaed
Dulitha Wijewantha <dulithaz@gmail.com>
2015-10-04 17:06:04 +0530

    * Added support for view user option * Added support for edit user option * Developed the back-end function to get a user * Developed the back-end function to update a user
    Refer JIRA:- https://wso2.org/jira/browse/MDM-388

--- modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/beans/UserWrapper.java
+++ modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/beans/UserWrapper.java
@@ -21,6 +21,10 @@ package org.wso2.carbon.mdm.beans;
 public class UserWrapper {
 
     private String username;
+    /*
+        Base64 encoded password
+     */
+    private String password;
     private String firstname;
     private String lastname;
     private String emailAddress;
@@ -64,5 +68,13 @@ public class UserWrapper {
 
     public void setRoles(String[] roles) {
         this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
