7eeb52eb1998197d455b2c879a97aef569807325
harshanl <harshan@wso2.com>
2015-10-06 18:37:32 +0530

    Added permission.xml  to webapps and refactored JAX-RS endpoints

--- modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/api/Policy.java
+++ modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/api/Policy.java
@@ -161,7 +161,7 @@ public class Policy {
         }
     }
 
-    @POST
+    @PUT
     @Path("{id}")
     public ResponsePayload updatePolicy(org.wso2.carbon.policy.mgt.common.Policy policy, @PathParam("id") int policyId)
             throws MDMAPIException {
