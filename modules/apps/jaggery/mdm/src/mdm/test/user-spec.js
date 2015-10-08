94ea28cd5ecf42cb5872c2c1463b99cbdde4c135
Dulitha Wijewantha <dulithaz@gmail.com>
2015-10-08 14:12:29 +0530

    Policy View & Edit * Load the persisted policy to the edit ui * Save perisisted policy (there is an issue in persisting)
    JIRA:- https://wso2.org/jira/browse/EMM-812

--- modules/apps/jaggery/mdm/src/mdm/test/user-spec.js
+++ modules/apps/jaggery/mdm/src/mdm/test/user-spec.js
@@ -16,51 +16,61 @@
  * under the License.
  */
 
-describe('Device Module', function () {
+describe('User Module', function () {
     var log = new Log();
-    var mobileDB;
-    var cdmDB;
-    var deviceModule = require("/modules/device.js").deviceModule;
     var userModule = require("/modules/user.js").userModule;
     var constants = require("/modules/constants.js");
     function tearUp() {
-        mobileDB = new Database("MobileDM_DS");
-        cdmDB = new Database("DM_DS");
-        cdmDB.query("insert into dm_device(description, name, date_of_enrollment, date_of_last_update, " +
-        "ownership,status, device_type_id, device_identification, owner, tenant_id ) " +
-        "values ('Galaxy Tab','Admin Samsung', 1425467382, 1425467382, 'BYOD', 'ACTIVE'," +
-        " 1,'4892813d-0b18-4a02-b7b1-61775257488e', 'admin@wso2.com', '-1234');");
-        cdmDB.query("insert into dm_device(description, name, date_of_enrollment, date_of_last_update, " +
-        "ownership,status, device_type_id, device_identification, owner, tenant_id ) " +
-        "values ('Galaxy Tab','Admin Samsung', 1425467382, 1425467382, 'BYOD', 'ACTIVE'," +
-        " 1,'4892813d-0b18-4a02-b7b1-61775257488F', 'mdm@wso2.com', '-1234');");
-
-        mobileDB.query("insert into mbl_device (mobile_device_id, push_token, imei ,imsi, os_version, " +
-        "device_model , vendor ,latitude ,longitude , challenge ,token, unlock_token ,serial ) " +
-        "values ('4892813d-0b18-4a02-b7b1-61775257488e', 'sdfsdf', 'cxv', 'vbcb', '4.1', " +
-        "'Galaxy Tab', 'Samsung',  '234234234', '4345345234234', 'dfjsdlfk', 'wuweir234', " +
-        "'ksdfjlskfjwer', '234234');");
-        mobileDB.query("insert into mbl_device (mobile_device_id, push_token, imei ,imsi, os_version, " +
-        "device_model , vendor ,latitude ,longitude , challenge ,token, unlock_token ,serial ) " +
-        "values ('4892813d-0b18-4a02-b7b1-61775257488F', 'sdfsdf', 'cxv', 'vbcb', '4.1', " +
-        "'Galaxy Tab', 'Samsung',  '234234234', '4345345234234', 'dfjsdlfk', 'wuweir234', " +
-        "'ksdfjlskfjwer', '234234');");
         session.put(constants.USER_SESSION_KEY, {"username" : "admin", "domain": "carbon.super", "tenantId": "-1234"});
     }
 
     function tearDown() {
         deleteData();
-        mobileDB.close();
-        cdmDB.close();
         session.put(constants.USER_SESSION_KEY, null);
     }
 
     function deleteData(){
-        cdmDB.query("delete from dm_device where device_identification='4892813d-0b18-4a02-b7b1-61775257488e'");
-        cdmDB.query("delete from dm_device where device_identification='4892813d-0b18-4a02-b7b1-61775257488F'");
-        mobileDB.query("delete from mbl_device where mobile_device_id='4892813d-0b18-4a02-b7b1-61775257488e'");
-        mobileDB.query("delete from mbl_device where mobile_device_id='4892813d-0b18-4a02-b7b1-61775257488F'");
     }
+
+    it('Get user', function () {
+        try {
+            tearUp();
+            var response = userModule.getUser("admin");
+            expect(response.status).toBe("success");
+        } catch (e) {
+            log.error(e);
+            throw e;
+        } finally {
+            tearDown();
+        }
+    });
+    /*
+     Not testable since the functionality moved to front end
+    it('Create user', function () {
+        try {
+            tearUp();
+            var statusCode = userModule.addUser("wso2user", "wso2", "corp", "wso2-no-reply@wso2.com", ["admin"]);
+            expect(statusCode).toBe(201);
+        } catch (e) {
+            log.error(e);
+            throw e;
+        } finally {
+            tearDown();
+        }
+    });
+
+    it('Update user', function () {
+        try {
+            tearUp();
+            var results = userModule.updat
+            expect(results.length).not.toBe(0);
+        } catch (e) {
+            log.error(e);
+            throw e;
+        } finally {
+            tearDown();
+        }
+    });
 
     it('List all users', function () {
         try {
@@ -85,4 +95,5 @@ describe('Device Module', function () {
             tearDown();
         }
     });
+    */
 });
