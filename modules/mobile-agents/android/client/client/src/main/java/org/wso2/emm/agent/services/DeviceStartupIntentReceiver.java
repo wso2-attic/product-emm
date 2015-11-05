9e4d5eaac1e48e79c0fa14d5d4c26c921ad11a06
Kasun Delgolla <kasun.mck@gmail.com>
2015-11-05 12:18:22 +0530

    Committing android agent - device startup crashing issue fix

--- modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/services/DeviceStartupIntentReceiver.java
+++ modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/services/DeviceStartupIntentReceiver.java
@@ -38,6 +38,8 @@ import android.os.SystemClock;
 public class DeviceStartupIntentReceiver extends BroadcastReceiver {
 	private static final int DEFAULT_TIME_MILLISECONDS = 1000;
 	private static final int DEFAULT_REQUEST_CODE = 0;
+	public static final int DEFAULT_INDEX = 0;
+	public static final int DEFAULT_INTERVAL = 30000;
 	private Resources resources;
 
 	@Override
@@ -54,8 +56,10 @@ public class DeviceStartupIntentReceiver extends BroadcastReceiver {
 		String mode =
 				Preference
 						.getString(context, resources.getString(R.string.shared_pref_notifier));
-		Float interval = Float.valueOf(Preference
-				                               .getString(context, resources.getString(R.string.shared_pref_frequency)).toString());
+		int interval = Preference.getInt(context, context.getResources().getString(R.string.shared_pref_frequency));
+		if(interval == DEFAULT_INDEX){
+			interval = DEFAULT_INTERVAL;
+		}
 
 		if (mode != null && Constants.NOTIFIER_LOCAL.equals(mode.trim().toUpperCase(Locale.ENGLISH))) {
 			long startTime = SystemClock.elapsedRealtime() + DEFAULT_TIME_MILLISECONDS;
@@ -69,7 +73,7 @@ public class DeviceStartupIntentReceiver extends BroadcastReceiver {
 			AlarmManager alarmManager =
 					(AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 			alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, startTime,
-					interval.intValue(), recurringAlarmIntent);
+					interval, recurringAlarmIntent);
 		}
 	}
 }
