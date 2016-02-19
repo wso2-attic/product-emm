41f8223bdd4f46577c5559d1df8ac62f13daf29c
dilee <dilee.dev@gmail.com>
2016-02-19 12:51:59 +0530

    Added CredentialManagementResponseBuilder and refactored user-related API classes

--- modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/api/User.java
+++ modules/apps/jax-rs/mdm-admin/src/main/java/org/wso2/carbon/mdm/api/User.java
@@ -29,6 +29,7 @@ import org.wso2.carbon.device.mgt.common.EmailMessageProperties;
 import org.wso2.carbon.device.mgt.common.PaginationRequest;
 import org.wso2.carbon.device.mgt.core.service.DeviceManagementProviderService;
 import org.wso2.carbon.mdm.api.common.MDMAPIException;
+import org.wso2.carbon.mdm.api.util.CredentialManagementResponseBuilder;
 import org.wso2.carbon.mdm.api.util.MDMAPIUtils;
 import org.wso2.carbon.mdm.api.util.ResponsePayload;
 import org.wso2.carbon.mdm.beans.UserCredentialWrapper;
@@ -761,28 +762,7 @@ public class User {
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response resetPassword(UserCredentialWrapper credentials) throws MDMAPIException {
-        UserStoreManager userStoreManager = MDMAPIUtils.getUserStoreManager();
-        ResponsePayload responsePayload = new ResponsePayload();
-        try {
-            byte[] decodedNewPassword = Base64.decodeBase64(credentials.getNewPassword());
-            byte[] decodedOldPassword = Base64.decodeBase64(credentials.getOldPassword());
-            userStoreManager.updateCredential(credentials.getUsername(), new String(
-                    decodedNewPassword, "UTF-8"), new String(decodedOldPassword, "UTF-8"));
-            responsePayload.setStatusCode(HttpStatus.SC_CREATED);
-            responsePayload.setMessageFromServer("User password by username: " + credentials.getUsername() +
-                                                 " was successfully changed.");
-            return Response.status(HttpStatus.SC_CREATED).entity(responsePayload).build();
-        } catch (UserStoreException e) {
-            String errorMsg = "Exception in trying to change the password by username: " + credentials.getUsername();
-            log.error(errorMsg, e);
-            responsePayload.setStatusCode(HttpStatus.SC_BAD_REQUEST);
-            responsePayload.setMessageFromServer("Old password does not match.");
-            return Response.status(HttpStatus.SC_BAD_REQUEST).entity(responsePayload).build();
-        } catch (UnsupportedEncodingException e) {
-            String errorMsg = "Exception in trying to change the password by username: " + credentials.getUsername();
-            log.error(errorMsg, e);
-            throw new MDMAPIException(errorMsg, e);
-        }
+        return CredentialManagementResponseBuilder.buildChangePasswordResponse(credentials);
     }
 
     /**
@@ -797,26 +777,6 @@ public class User {
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response resetPasswordByAdmin(UserCredentialWrapper credentials) throws MDMAPIException {
-        UserStoreManager userStoreManager = MDMAPIUtils.getUserStoreManager();
-        ResponsePayload responsePayload = new ResponsePayload();
-        try {
-            byte[] decodedNewPassword = Base64.decodeBase64(credentials.getNewPassword());
-            userStoreManager.updateCredentialByAdmin(credentials.getUsername(), new String(
-                    decodedNewPassword, "UTF-8"));
-            responsePayload.setStatusCode(HttpStatus.SC_CREATED);
-            responsePayload.setMessageFromServer("User password by username: " + credentials.getUsername() +
-                    " was successfully changed.");
-            return Response.status(HttpStatus.SC_CREATED).entity(responsePayload).build();
-        } catch (UserStoreException e) {
-            String errorMsg = "Exception in trying to change the password by username: " + credentials.getUsername();
-            log.error(errorMsg, e);
-            responsePayload.setStatusCode(HttpStatus.SC_BAD_REQUEST);
-            responsePayload.setMessageFromServer("Could not change the password.");
-            return Response.status(HttpStatus.SC_BAD_REQUEST).entity(responsePayload).build();
-        } catch (UnsupportedEncodingException e) {
-            String errorMsg = "Exception in trying to change the password by username: " + credentials.getUsername();
-            log.error(errorMsg, e);
-            throw new MDMAPIException(errorMsg, e);
-        }
+        return CredentialManagementResponseBuilder.buildResetPasswordResponse(credentials);
     }
 }
