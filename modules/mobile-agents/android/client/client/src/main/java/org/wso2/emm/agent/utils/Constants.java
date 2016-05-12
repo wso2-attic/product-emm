db0c04d230558c3ba2a0fe5ff11b7e5be6e099b5
Kasun Delgolla <kasun.mck@gmail.com>
2016-05-12 20:13:14 +0530

    Committing agent app downloader logic changes according to APPM changes

--- modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/utils/Constants.java
+++ modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/utils/Constants.java
@@ -47,6 +47,7 @@ public class Constants {
 	// This is set to override the server host name retrieving screen. If overriding is not
 	// needed, set this to null.
 	public static final String DEFAULT_HOST = null;
+	public static final String APP_MANAGER_HOST = null;
 	public static final String SYSTEM_SERVICE_PACKAGE = "org.wso2.emm.system.service";
 	public static final String AGENT_PACKAGE = "org.wso2.emm.agent";
 
@@ -70,6 +71,7 @@ public class Constants {
 	public static final String GOOGLE_PLAY_APP_URI = "market://details?id=";
 	public static final String DYNAMIC_CLIENT_REGISTER_ENDPOINT = "/dynamic-client-web/register";
 	public static final String APP_LIST_ENDPOINT = "/api/appm/publisher/v1.0/apps/mobileapp?field-filter=all";
+	public static final String APP_DOWNLOAD_ENDPOINT = "/api/appm/store/v1.0/apps/mobile/binaries";
 	public static final String ACTION_RESPONSE = "org.wso2.emm.agent.MESSAGE_PROCESSED";
 	public static final String POLICY_ENDPOINT = SERVER_APP_ENDPOINT + "policy/";
 	public static final String EVENT_ENDPOINT = SERVER_APP_ENDPOINT + "events/";
