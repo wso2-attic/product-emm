7ddee928be88b3eed4428560fd32f882dd4e9009
madhawap <madhawa.818@gmail.com>
2016-06-14 12:38:52 +0530

    changes to the UI, base on new API changes

--- modules/apps/jaggery/emm/src/emm/modules/device.js
+++ modules/apps/jaggery/emm/src/emm/modules/device.js
@@ -271,7 +271,7 @@ deviceModule = function () {
         try {
             utility.startTenantFlow(carbonUser);
             //var url = mdmProps["httpsURL"] + "/mdm-admin/devices/view?type=" + deviceType + "&id=" + deviceId;
-            var url = mdmProps["httpsURL"] + "  /api/device-mgt/v1.0/devices/" + deviceType + "/" + deviceId;
+            var url = mdmProps["httpsURL"] + "/api/device-mgt/v1.0/devices/" + deviceType + "/" + deviceId;
             var dataNew = serviceInvokers.XMLHttp.get(
                 url, function (responsePayload) {
                     var device = responsePayload.responseContent;
