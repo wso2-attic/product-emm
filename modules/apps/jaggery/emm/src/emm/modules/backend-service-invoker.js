fdbf3aa4b10313a4cb09bff52a3b6db50c706006
dilanua <dilan.u.ariyaratne@gmail.com>
2016-06-14 16:22:03 +0530

    Fixing Jaggery REQUEST/RESPONSE interceptor

--- modules/apps/jaggery/emm/src/emm/modules/backend-service-invoker.js
+++ modules/apps/jaggery/emm/src/emm/modules/backend-service-invoker.js
@@ -28,13 +28,13 @@ var backendServiceInvoker = function () {
     var privateMethods = {};
     var publicWSInvokers = {};
 
-    var IS_OAUTH_ENABLED = true;
     var TOKEN_EXPIRED = "Access token expired";
     var TOKEN_INVALID = "Invalid input. Access token validation failed";
 
+    var mdmProps = require("/config/mdm-props.js").config();
     var constants = require("/modules/constants.js");
+    var userModule = require("/modules/user.js")["userModule"];
     var tokenUtil = require("/modules/api-wrapper-util.js")["apiWrapperUtil"];
-    var mdmProps = require('/config/mdm-props.js').config();
 
     /**
      * This method reads the token pair from the session and return the access token.
@@ -48,6 +48,12 @@ var backendServiceInvoker = function () {
             return null;
         }
     };
+
+    /**
+     * ---------------------------------------------------------------------------
+     * Start of XML-HTTP-REQUEST based Interceptor implementations
+     * ---------------------------------------------------------------------------
+     */
 
     /**
      * This method add Oauth authentication header to outgoing XML-HTTP Requests if Oauth authentication is enabled.
@@ -64,12 +70,15 @@ var backendServiceInvoker = function () {
         xmlHttpRequest.setRequestHeader(constants["CONTENT_TYPE_IDENTIFIER"], constants["APPLICATION_JSON"]);
         xmlHttpRequest.setRequestHeader(constants["ACCEPT_IDENTIFIER"], constants["APPLICATION_JSON"]);
 
-        if (IS_OAUTH_ENABLED) {
+        if (mdmProps["isOAuthEnabled"]) {
             var accessToken = privateMethods.getAccessToken();
             if (!accessToken) {
-                response.sendRedirect(mdmProps["httpsURL"] + "/emm/login");
+                userModule.logout(function () {
+                    response.sendRedirect(mdmProps["appContext"] + "login");
+                });
             } else {
-                xmlHttpRequest.setRequestHeader(constants["AUTHORIZATION_HEADER"], constants["BEARER_PREFIX"] + accessToken);
+                xmlHttpRequest.
+                    setRequestHeader(constants["AUTHORIZATION_HEADER"], constants["BEARER_PREFIX"] + accessToken);
             }
         }
 
@@ -102,9 +111,7 @@ var backendServiceInvoker = function () {
      * @param responseCallback a function to be called with response retrieved.
      */
     privateMethods.initiateXMLHTTPRequest = function (httpMethod, requestPayload, endpoint, responseCallback) {
-        if (privateMethods.getAccessToken()) {
-            return privateMethods.execute(httpMethod, requestPayload, endpoint, responseCallback, 0);
-        }
+        return privateMethods.execute(httpMethod, requestPayload, endpoint, responseCallback, 0);
     };
 
     /**
@@ -148,6 +155,12 @@ var backendServiceInvoker = function () {
     };
 
     /**
+     * ---------------------------------------------------------------------------
+     * Start of WS-REQUEST based Interceptor implementations
+     * ---------------------------------------------------------------------------
+     */
+
+    /**
      * This method add Oauth authentication header to outgoing WS Requests if Oauth authentication is enabled.
      * @param action
      * @param endpoint service end point to be triggered.
@@ -160,7 +173,7 @@ var backendServiceInvoker = function () {
         var ws = require("ws");
         var wsRequest = new ws.WSRequest();
         var options = [];
-        if (IS_OAUTH_ENABLED) {
+        if (mdmProps["isOAuthEnabled"]) {
             var accessToken = privateMethods.getAccessToken();
             if (accessToken) {
                 var authenticationHeaderName = String(constants["AUTHORIZATION_HEADER"]);
@@ -196,15 +209,21 @@ var backendServiceInvoker = function () {
     /**
      * This method invokes return initiateWSRequest for soap calls
      * @param action describes particular soap action.
+     * @param requestPayload SOAP request payload which is needed to be send.
      * @param endpoint service end point to be triggered.
-     * @param payload soap payload which need to be send.
      * @param successCallback a function to be called if the respond if successful.
      * @param errorCallback a function to be called if en error is reserved.
      * @param soapVersion soapVersion which need to used.
      */
-    publicWSInvokers.soapRequest = function (action, endpoint, payload, successCallback, errorCallback, soapVersion) {
-        return privateMethods.initiateWSRequest(action, endpoint, successCallback, errorCallback, soapVersion, payload);
+    publicWSInvokers.soapRequest = function (action, requestPayload, endpoint, successCallback, errorCallback, soapVersion) {
+        return privateMethods.initiateWSRequest(action, requestPayload, endpoint, successCallback, errorCallback, soapVersion);
     };
+
+    /**
+     * ---------------------------------------------------------------------------
+     * Start of HTTP-CLIENT-REQUEST based Interceptor implementations
+     * ---------------------------------------------------------------------------
+     */
 
     /**
      * This method add Oauth authentication header to outgoing HTTPClient Requests if Oauth authentication is enabled.
@@ -246,7 +265,8 @@ var backendServiceInvoker = function () {
         header.setName(constants["ACCEPT_IDENTIFIER"]);
         header.setValue(constants["APPLICATION_JSON"]);
         httpMethodObject.addRequestHeader(header);
-        if (IS_OAUTH_ENABLED) {
+
+        if (mdmProps["isOAuthEnabled"]) {
             var accessToken = privateMethods.getAccessToken();
             if (accessToken) {
                 header = new Header();
@@ -323,10 +343,10 @@ var backendServiceInvoker = function () {
             initiateHTTPClientRequest(constants["HTTP_DELETE"], url, successCallback, errorCallback, requestPayload);
     };
 
-    var publicInvokers = {};
-    publicInvokers.XMLHttp = publicXMLHTTPInvokers;
-    publicInvokers.WS = publicWSInvokers;
-    publicInvokers.HttpClient = publicHTTPClientInvokers;
+    var publicMethods = {};
+    publicMethods.XMLHttp = publicXMLHTTPInvokers;
+    publicMethods.WS = publicWSInvokers;
+    publicMethods.HttpClient = publicHTTPClientInvokers;
 
-    return publicInvokers;
+    return publicMethods;
 }();
