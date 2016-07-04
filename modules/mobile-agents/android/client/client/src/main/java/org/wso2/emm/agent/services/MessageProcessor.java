6c2b4180df2122ad0eca25c715dfe75164ebf399
Kasun Delgolla <kasun.mck@gmail.com>
2016-07-04 14:15:22 +0530

    Committing application installation status reporting

--- modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/services/MessageProcessor.java
+++ modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/services/MessageProcessor.java
@@ -196,6 +196,8 @@ public class MessageProcessor implements APIResultCallBack {
 				}
 				Preference.putString(context, context.getResources().getString(
 						R.string.firmware_upgrade_failed_message), null);
+				Preference.putInt(context, context.getResources().getString(
+						R.string.firmware_upgrade_failed_id), 0);
 			}
 
 			int applicationOperationId = Preference.getInt(context, context.getResources().getString(
@@ -219,6 +221,14 @@ public class MessageProcessor implements APIResultCallBack {
 					replyPayload = new ArrayList<>();
 					replyPayload.add(applicationOperation);
 				}
+				Preference.putString(context, context.getResources().getString(
+						R.string.app_install_code), null);
+				Preference.putString(context, context.getResources().getString(
+						R.string.app_install_status), null);
+				Preference.putString(context, context.getResources().getString(
+						R.string.app_install_failed_message), null);
+				Preference.putInt(context, context.getResources().getString(
+						R.string.app_install_id), 0);
 			}
 			requestParams =  mapper.writeValueAsString(replyPayload);
 		} catch (JsonMappingException e) {
