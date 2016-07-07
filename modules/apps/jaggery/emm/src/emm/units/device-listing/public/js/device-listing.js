62ddfe4beade6b5e3c79a48bdc314e9a5d1e3743
prabathabey <prabathabey@git.com>
2016-07-07 17:31:33 +0530

    Fixing grammar issues in the info message displayed when no device is available to be displayed

--- modules/apps/jaggery/emm/src/emm/units/device-listing/public/js/device-listing.js
+++ modules/apps/jaggery/emm/src/emm/units/device-listing/public/js/device-listing.js
@@ -441,7 +441,7 @@ function initPage() {
                     loadDevices();
                 } else {
                     $("#loading-content").remove();
-                    $("#device-listing-status-msg").text("No enrolled devices found.");
+                    $("#device-listing-status-msg").text("No enrolled device is found.");
                     $("#device-listing-status").removeClass(' hidden');
                 }
             }
