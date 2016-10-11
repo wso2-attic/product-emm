b1339d4c87c71aee32be99dea291ada182662ecd
harshanl <harshan@wso2.com>
2016-10-11 16:49:47 +0530

    EMM-1731:Removed duplicate alert box

--- modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/GCMBroadcastReceiver.java
+++ modules/mobile-agents/android/client/client/src/main/java/org/wso2/emm/agent/GCMBroadcastReceiver.java
@@ -22,8 +22,7 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.WakefulBroadcastReceiver;
-import android.util.Log;
+
 import com.google.android.gms.gcm.GcmReceiver;
 
 /**
